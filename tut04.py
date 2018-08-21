5606e96207de127ded1f366fcc6eb8a818befbcc
Javi Vazz <developer.javi.vazz@gmail.com>
2018-08-21 17:14:34 -0500

    Finish tutorial Cherrypy

--- tut04.py
+++ tut04.py
@@ -7,7 +7,15 @@ import cherrypy
 class StringGenerator(object):
     @cherrypy.expose
     def index(self):
-        return "Hello world!"
+        return """<html>
+          <head></head>
+          <body>
+            <form method="get" action="generate">
+              <input type="text" value="8" name="length" />
+              <button type="submit">Give it now!</button>
+            </form>
+          </body>
+        </html>"""
 
     @cherrypy.expose
     def generate(self, length=8):
